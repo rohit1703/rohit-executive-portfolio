d76e53fff1522f322cbac93d431093ccba03b430
Rohit <mallavarapurohit@gmail.com>
2025-12-30 18:26:31 +0530

    V2 Mobile formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 
 import '@fontsource/outfit/700.css';
-import '@fontsource/outfit/800.css';
 import '@fontsource/outfit/900.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
